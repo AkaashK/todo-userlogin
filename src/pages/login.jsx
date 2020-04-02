1030c744e74f844b2f8d9893b72f4f071b32ed58
Ak_ubuntu <akaash324it@gmail.com>
2020-04-02 13:32:43 +0530

    initial commit

---
+++ src/pages/login.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import logo from '../logo.svg'
+import '../App.css'
+
+function login() {
+
+    const user = [{
+        email: 'hello@example.com',
+        password: '123456',
+    }]
+
+    function UserLogin() {
+        const email = document.getElementById('email').value
+        const password = document.getElementById('password').value
+
+        if (email === '' && password === '') {
+            alert('Please enter all the fields')
+        } else {
+            let newUser = [...user]
+            let isValiduser = newUser.find((user) => password === user.password && email === user.email)
+
+            if (isValiduser) {
+                alert('Welcome')
+            } else {
+                document.getElementById('email').value = ''
+                document.getElementById('password').value = ''
+                alert('No user')
+            }
+        }
+    }
+
+    return (
+        <div className="App-header">
+            <img src={logo} className="App-logo" alt=""></img>
+            <h2 className="title">LOGIN</h2>
+            email<input id="email" type="text" required /> <br />
+            password<input id="password" type="password" required /> <br />
+            <button className="btn" type="button" onClick={UserLogin}>
+                login
+            </button>
+        </div>
+    )
+}
+export default login;
